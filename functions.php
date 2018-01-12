efbddb506b620fd993f15e4a1a48affdbfc5f5ba
Ron Davis <rdavis@reactuatesoftware.com>
2018-01-12 11:34:46 -0600

    Fixed create function error. Pulled in line ending stuff too.

--- functions.php
+++ functions.php
@@ -57,9 +57,9 @@ function podcaster_setup() {
 		'quote',
 		'link',
 	) );
-    
+
     add_theme_support( 'custom-logo' );
-    
+
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'podcaster_custom_background_args', array(
 		'default-color' => 'ffffff',
@@ -77,7 +77,7 @@ add_filter( 'post_thumbnail_html', 'podcaster_post_image_html', 10, 3 );
     }
 
     endif; // podcaster_setup
-    add_action( 'after_setup_theme', 'podcaster_setup' ); 
+    add_action( 'after_setup_theme', 'podcaster_setup' );
 
     // Register Nav Walker class_alias
     require_once('wp_bootstrap_navwalker.php');
@@ -115,7 +115,7 @@ add_action('after_setup_theme','podcaster_theme_setup');
 
 // Podscaster Scripts
 function podcaster_scripts() {
-    
+
 	wp_enqueue_script( 'firstheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
     wp_enqueue_script( 'firstheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 }
@@ -144,7 +144,7 @@ function podcaster_init_widgets($id){
         'before_title' => '<h3 class="widget-title">',
         'after_title' => '</h3>',
     ) );
- 
+
     // Second Footer Widget Area, located above the bottom footer. Empty by default.
     register_sidebar( array(
         'name' => __( 'Second Footer Widget Area', 'podcaster' ),
@@ -155,7 +155,7 @@ function podcaster_init_widgets($id){
         'before_title' => '<h3 class="widget-title">',
         'after_title' => '</h3>',
     ) );
- 
+
     // Third Footer Widget Area, located above the bottom footer. Empty by default.
     register_sidebar( array(
         'name' => __( 'Third Footer Widget Area', 'podcaster' ),
@@ -166,7 +166,7 @@ function podcaster_init_widgets($id){
         'before_title' => '<h3 class="widget-title">',
         'after_title' => '</h3>',
     ) );
- 
+
     // Fourth Footer Widget Area, located above the bottom footer. Empty by default.
     register_sidebar( array(
         'name' => __( 'Fourth Footer Widget Area', 'podcaster' ),
@@ -251,12 +251,12 @@ add_action( 'init', 'change_post_object_label' );
 
 function pagination_bar() {
     global $wp_query;
- 
+
     $total_pages = $wp_query->max_num_pages;
- 
+
     if ($total_pages > 1){
         $current_page = max(1, get_query_var('paged'));
- 
+
         echo paginate_links(array(
             'base' => get_pagenum_link(1) . '%_%',
             'format' => '/page/%#%',
@@ -277,6 +277,160 @@ function show_template() {
     if( is_super_admin() ){
         global $template;
         print_r($template);
-    } 
+    }
 }
 
+// Theme updater code for Github. Credit to Douglas Beck, UCF Web Communications.
+// register the custom stylesheet header
+add_action( 'extra_theme_headers', 'github_extra_theme_headers' );
+function github_extra_theme_headers( $headers ) {
+    $headers['Github Theme URI'] = 'Github Theme URI';
+    return $headers;
+}
+add_filter('site_transient_update_themes', 'transient_update_themes_filter');
+function transient_update_themes_filter($data){
+	global $wp_version;
+	$wp_34 = version_compare($wp_version, '3.4', '>=');
+	$installed_themes = $wp_34 ? wp_get_themes() : get_themes();
+	foreach ( (array) $installed_themes as $theme_title => $_theme ) {
+		// the WP_Theme object is very different now...
+		// This whole function should be refactored to not directly
+		// rely on the $theme variable the way it does
+		if($wp_34) {
+			if(!$_theme->get('Github Theme URI')) {
+				continue;
+			} else {
+				$theme = array(
+					'Github Theme URI' => $_theme->get('Github Theme URI'),
+					'Stylesheet'       => $_theme->stylesheet,
+					'Version'          => $_theme->version
+				);
+			}
+		} else {
+			// get the Github URI header, skip if not set
+			$theme = $_theme;
+			if(isset($theme['Stylesheet Files'][0]) && is_readable($theme['Stylesheet Files'][0])){
+				$stylesheet = $theme['Stylesheet Dir'] . '/style.css';
+
+				$theme_data = get_theme_data($stylesheet);
+				if(empty($theme_data['Github Theme URI'])){
+					continue;
+				} else {
+					$theme['Github Theme URI'] = $theme_data['Github Theme URI'];
+				}
+			};
+		}
+
+		$theme_key = $theme['Stylesheet'];
+
+		// Add Github Theme Updater to return $data and hook into admin
+		remove_action( "after_theme_row_" . $theme['Stylesheet'], 'wp_theme_update_row');
+		add_action( "after_theme_row_" . $theme['Stylesheet'], 'github_theme_update_row', 11, 2 );
+
+		// Grab Github Tags
+		preg_match(
+			'/http(s)?:\/\/github.com\/(?<username>[\w-]+)\/(?<repo>[\w-]+)$/',
+			$theme['Github Theme URI'],
+			$matches);
+		if(!isset($matches['username']) or !isset($matches['repo'])){
+			$data->response[$theme_key]['error'] = 'Incorrect github project url.  Format should be (no trailing slash): <code style="background:#FFFBE4;">https://github.com/&lt;username&gt;/&lt;repo&gt;</code>';
+			continue;
+		}
+		$url = sprintf('https://api.github.com/repos/%s/%s/tags', urlencode($matches['username']), urlencode($matches['repo']));
+
+		$response = get_transient(md5($url)); // Note: WP transients fail if key is long than 45 characters
+		if(empty($response)){
+			$raw_response = wp_remote_get($url, array('sslverify' => false, 'timeout' => 10));
+			if ( is_wp_error( $raw_response ) ){
+				$data->response[$theme_key]['error'] = "Error response from " . $url;
+				continue;
+			}
+			$response = json_decode($raw_response['body']);
+			if(isset($response->message)){
+				if(is_array($response->message)){
+					$errors = '';
+					foreach ( $response->message as $error) {
+						$errors .= ' ' . $error;
+					}
+				} else {
+					$errors = print_r($response->message, true);
+				}
+				$data->response[$theme_key]['error'] = sprintf('While <a href="%s">fetching tags</a> api error</a>: <span class="error">%s</span>', $url, $errors);
+				continue;
+			}
+
+			if(count($response) == 0){
+				$data->response[$theme_key]['error'] = "Github theme does not have any tags";
+				continue;
+			}
+
+			//set cache, just 60 seconds
+			set_transient(md5($url), $response, 30);
+		}
+
+		// Sort and get latest tag
+    $tags = array_map(function($in_response){return $in_response->name;}, $response);
+    usort($tags, "version_compare");
+
+
+		// check for rollback
+		if(isset($_GET['rollback'])){
+			$data->response[$theme_key]['package'] =
+				$theme['Github Theme URI'] . '/zipball/' . urlencode($_GET['rollback']);
+			continue;
+		}
+
+
+		// check and generate download link
+		$newest_tag = array_pop($tags);
+    #error_log("After tags {".implode(", ",$tags)."}");
+		if(version_compare($theme['Version'],  $newest_tag, '>=')){
+			// up-to-date!
+			$data->up_to_date[$theme_key]['rollback'] = $tags;
+			continue;
+		}
+
+
+		// new update available, add to $data
+		$download_link = $theme['Github Theme URI'] . '/zipball/' . $newest_tag;
+		$update = array();
+		$update['new_version'] = $newest_tag;
+		$update['url']         = $theme['Github Theme URI'];
+		$update['package']     = $download_link;
+		$data->response[$theme_key] = $update;
+
+    error_log("DL link: ".$download_link.", github URI: ".$theme['Github Theme URI']);
+
+
+	}
+
+	return $data;
+}
+add_filter('upgrader_source_selection', 'upgrader_source_selection_filter', 10, 3);
+function upgrader_source_selection_filter($source, $remote_source=NULL, $upgrader=NULL){
+	/*
+		Github delivers zip files as <Username>-<TagName>-<Hash>.zip
+		must rename this zip file to the accurate theme folder
+	*/
+  error_log("Source: ".$source." remote source:".$remote_source." theme:".$upgrader->skin->theme);
+	if(isset($source, $remote_source, $upgrader->skin->theme)){
+		$corrected_source = $remote_source . '/' . $upgrader->skin->theme . '/';
+		if(@rename($source, $corrected_source)){
+			return $corrected_source;
+		} else {
+			$upgrader->skin->feedback("Unable to rename downloaded theme.");
+			return new WP_Error();
+		}
+	}
+	return $source;
+}
+/*
+   Function to address the issue that users in a standalone WordPress installation
+   were receiving SSL errors and were unable to install themes.
+   https://github.com/UCF/Theme-Updater/issues/3
+*/
+add_action('http_request_args', 'no_ssl_http_request_args', 10, 2);
+function no_ssl_http_request_args($args, $url) {
+	$args['sslverify'] = false;
+	return $args;
+}
