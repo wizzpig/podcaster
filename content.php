39bfe1d0c44c895879dd8afea24e7bcfe4a5e1c2
Ron Davis <rdavis@reactuatesoftware.com>
2018-03-01 11:01:50 -0600

    Added space between date and 'by'

--- content.php
+++ content.php
@@ -7,7 +7,7 @@
             <?php endif; ?>
         </h2>
         
-        <p class="blog-post-meta"><?php the_time('F j, Y g:i a'); ?>by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a></p>
+        <p class="blog-post-meta"><?php the_time('F j, Y g:i a'); ?> by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a></p>
         <?php if(has_post_thumbnail()) : ?>
             <div class="post-thumb">
             <?php the_post_thumbnail(); ?>
